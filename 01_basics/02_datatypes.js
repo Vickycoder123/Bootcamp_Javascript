2cf8a9351d3917658665b466c6936a3392551035
Vikaskumar Gupta <121336664+Vickycoder123@users.noreply.github.com>
2023-10-25 13:48:30 +0000

    datatypes file added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+//Datatypes => nn bb ss o
+//number = 2
+//null is a standalone value or object
+//boolean = true/false
+//bigInt
+//string = ""
+//symbol => unique
+//object
+
+console.log(typeof 6)
+console.log(typeof null)// return object
+console.log(typeof undefined)
